7878e114a9aad2e2e02dc77a860cf088b094d507
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-10-01 08:32:09 -0300

    206. Reverse Linked List

---
+++ 206. Reverse Linked List/script.js
@@ -0,0 +1,24 @@
+var reverseList = function(head) {
+  let cur = head;
+  let prev = null;
+  let next;
+
+  while(cur !== null){
+      next = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = next
+  }
+  return prev
+};
+
+// ou
+
+/*
+var reverseList = function(head) {
+  let cur = head.reverse();
+  
+  return cur
+};
+
+*/
